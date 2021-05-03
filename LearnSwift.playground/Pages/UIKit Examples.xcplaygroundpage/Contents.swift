b04a8ab576faea74359d4d4462b7578b48afd5e1
choiyonghyeon <choiyh0601@naver.com>
2021-05-03 11:57:59 +0900

    Initial Commit

---
+++ LearnSwift.playground/Pages/UIKit Examples.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+//: [Previous](@previous)
+
+import UIKit
+
+let myLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
+
+myLabel.backgroundColor = UIColor.white
+myLabel.text = "Hello Swift"
+myLabel.textAlignment = .center
+myLabel.font = UIFont(name: "Georgia", size: 24)
+myLabel
+
+//: [Next](@next)
---
+++ LearnSwift.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+// print("Welcome to Swift")
+// print(greeting)
+/*
+var x = 10
+
+for index in 1...20 {
+    print("step", index)
+    print("x :", x)
+    let y = index * x
+    x -= 1
+    print("y :", y)
+    print("")
+}
+ */
+
+/*:
+ # Welcome to Playgrounds
+ This is your *first* playground which is intended to demonstrate:
+ * The use of **quick Look**
+ * Placing results **in-line** with the code
+ */
